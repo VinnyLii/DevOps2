f67f678f008cc6634dbb1a4511f420e8754483bb
VinnyLii <57904812+VinnyLii@users.noreply.github.com>
2023-05-13 14:11:35 +1000

    Updated test_testsFile.py

--- tests/test_testsFile.py
+++ tests/test_testsFile.py
@@ -1,17 +1,33 @@
-import subprocess
+import datetime
+import unittest
 
-def run_application():
-    subprocess.run(["python", "TimeMgt.py"])
+from main import Course, Assignment, print_courses
 
-def run_tests():
-    subprocess.run(["python", "test_TimeMgt.py"])
 
-def main():
-    print("Running application...")
-    run_application()
-    print("Running tests...")
-    run_tests()
-    print("Build completed successfully!")
+class TestCourseAndAssignment(unittest.TestCase):
+    
+    def setUp(self):
+        self.course = Course('DevOps2', datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=30), 'DEVOPS2-101')
+        self.assignment = Assignment('test assignment', datetime.datetime.now() + datetime.timedelta(days=10), '3 days')
+    
+    def test_course_has_name(self):
+        self.assertEqual(self.course.name, 'DevOps2')
+        
+    def test_course_has_code(self):
+        self.assertEqual(self.course.Course_code, 'DEVOPS2-101')
+        
+    def test_assignment_has_name(self):
+        self.assertEqual(self.assignment.name, 'test assignment')
+        
+    def test_assignment_mark_completed(self):
+        self.assertFalse(self.assignment.completed)
+        self.assignment.mark_completed()
+        self.assertTrue(self.assignment.completed)
+        
+    def test_print_courses(self):
+        courses = [self.course]
+        expected_output = '1. DevOps2'
+        self.assertEqual(print_courses(courses), expected_output)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    unittest.main()
